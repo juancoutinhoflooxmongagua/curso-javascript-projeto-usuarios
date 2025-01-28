921be6a6b79c143905e779b5ddaeb3c124431b51
Juan Coutinho <juan.coutinho.flooxmongagua@gmail.com>
2025-01-28 11:51:06 -0300

    feat: add for each

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,9 +1,33 @@
-var name = document.querySelector("#exampleInputName")
-var name = document.querySelectoAll("#form-user-create [name-gender]:checked")
-var name = document.querySelector("#exampleInputGenderF")
-var name = document.querySelector("#exampleInputBirth")
-var name = document.querySelector("#exampleInputCountry")
-var name = document.querySelector("#exampleInputEmail")
-var name = document.querySelector("#exampleInputPassword")
-var name = document.querySelector("#exampleInputFile")
-var name = document.querySelector("#exampleInputAdmin")
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelector("#form-user-create [name=gender]:checked");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#exampleInputAdmin");
+
+
+
+
+var fields = document.querySelectorAll("#form-user-create [name]")
+
+fields.forEach(function(field, index){
+
+    if (field.name === "gender") {
+
+        if (field.checked) {
+
+            console.log("SIM", field)
+            
+        }
+
+    } else {
+
+        console.log("NÃO")
+
+    }
+
+    // console.log(field.id, field.name, field.value, field.checked, index)
+
+});
