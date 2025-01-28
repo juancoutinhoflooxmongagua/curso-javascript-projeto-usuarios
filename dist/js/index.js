0a5704def047cc028fd7c9b44557490a7f467ad4
Juan Coutinho <juan.coutinho.flooxmongagua@gmail.com>
2025-01-28 12:22:58 -0300

    feat: add user json for management users

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,16 +1,5 @@
-var name = document.querySelector("#exampleInputName");
-var gender = document.querySelector("#form-user-create [name=gender]:checked");
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");
-var password = document.querySelector("#exampleInputPassword");
-var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
-
-
-
-
 var fields = document.querySelectorAll("#form-user-create [name]")
+var user = {};
 
 fields.forEach(function(field, index){
 
@@ -18,16 +7,14 @@ fields.forEach(function(field, index){
 
         if (field.checked) {
 
-            console.log("SIM", field)
-            
+            user[field.name] = field.value
         }
 
     } else {
 
-        console.log("NÃO")
-
+        user[field.name] = field.value
     }
 
-    // console.log(field.id, field.name, field.value, field.checked, index)
-
 });
+
+console.log(user);
