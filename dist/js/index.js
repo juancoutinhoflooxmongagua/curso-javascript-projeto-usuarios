3a37ada6601da84220f589d421705eadb646ed49
Juan Coutinho <juan.coutinho.flooxmongagua@gmail.com>
2025-01-29 10:09:37 -0300

    feat: add events for button

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,20 +1,21 @@
-var fields = document.querySelectorAll("#form-user-create [name]")
+var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
-fields.forEach(function(field, index){
-
+fields.forEach(function(field, index) {
     if (field.name === "gender") {
-
         if (field.checked) {
-
-            user[field.name] = field.value
+            user[field.name] = field.value;
         }
-
     } else {
-
-        user[field.name] = field.value
+        user[field.name] = field.value;
     }
-
 });
 
 console.log(user);
+
+document.querySelectorAll("button").forEach(function(btn) {
+    btn.addEventListener("click", function() {
+        // Your click handling logic here
+        console.log("Button clicked");
+    });
+});
